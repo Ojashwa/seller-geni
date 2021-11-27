16203f92e68401ab2f50a246801ea0439e9dcce6
Ojashwa Saini <sojs220@gmail.com>
2021-11-27 11:23:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isCreateButton, updateCreaeButton] = useState(true);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {isCreateButton && (
+          <button className="button"
+            onClick={() => {
+              updateCreaeButton(!isCreateButton);
+            }}
+          >
+            Create catalogue
+          </button>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
